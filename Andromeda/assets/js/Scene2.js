ddb60b1c82b3e4e59c2e1f4e125ba6e847ed721a
M0HX <M0HX@users.noreply.github.com>
2023-11-22 12:38:57 +0300

    Add files via upload

--- Andromeda/assets/js/Scene2.js
+++ Andromeda/assets/js/Scene2.js
@@ -24,144 +24,56 @@ class Scene2 extends Phaser.Scene {
         // this.cameras.main.startFollow(this.ship1);
 
 
+        // Add player with its animation at the end of create function
+        this.player = this.physics.add.sprite(config.width / 2 - 8, config.height - 64, "player");
+        this.player.play("thrust");
 
-        // Add ships to scene2 with key values.
-        // this.ship1 = this.add.image(config.width/2 - 50, config.height/2, "ship");
-        // this.ship2 = this.add.image(config.width/2, config.height/2, "ship2");
-        // this.ship3 = this.add.image(config.width/2 + 50, config.height/2, "ship3");
-        
-        // Change Image to Sprite
-        this.ship1 = this.add.sprite(config.width/2 - 50, config.height/2, "ship");
-        this.ship2 = this.add.sprite(config.width/2, config.height/2, "ship2");
-        this.ship3 = this.add.sprite(config.width/2 + 50, config.height/2, "ship3");
-        
+        // Create variable to listen to "Keyboard Events" 
+        this.cursorKeys = this.input.keyboard.createCursorKeys();
 
-        // Animate the sprite
+        // Set boundaries for Player's ship!
+        this.player.setCollideWorldBounds(true);
 
-        //Ship 1 Animte
-        this.anims.create({
-            key: "ship1_anim",
-            frames: this.anims.generateFrameNumbers("ship"),
-            frameRate: 20,
-            repeat: -1 // loop forever
-        });
+        // Assign a key so that the player can shoot!
+        this.spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
 
-        //Ship 2 Animte
-        this.anims.create({
-            key: "ship2_anim",
-            frames: this.anims.generateFrameNumbers("ship2"),
-            frameRate: 20,
-            repeat: -1 // loop forever
-        });
-
-        //Ship 3 Animte
-        this.anims.create({
-            key: "ship3_anim",
-            frames: this.anims.generateFrameNumbers("ship3"),
-            frameRate: 20,
-            repeat: -1 // loop forever
-        });
-
-        //Explosion Animte
-        this.anims.create({
-            key: "explode",
-            frames: this.anims.generateFrameNumbers("explosion"),
-            frameRate: 20,
-            repeat: 0,
-            hideOnComplete: true // so it disappear when it completes, runonce basically.
-        });
-
-        // Test ships animation
-        this.ship1.play("ship1_anim");
-        this.ship2.play("ship2_anim");
-        this.ship3.play("ship3_anim");
-
-        // Make each ship interactive (to destroy when clicked on! and explosion)
-        this.ship1.setInteractive();
-        this.ship2.setInteractive();
-        this.ship3.setInteractive();
-
-        // Add event listener to listens whenever an interactive object is clicked. //destory
-        this.input.on('gameobjectdown', this.destroyShip, this);
-
-
-        //scale image
-        this.ship1.setScale(2);
-
-        // flip y image
-        this.ship1.flipY = true;
-
-        // Add text to canvas
-        this.add.text(20, 20, "Playing Game", {font: "25px Arial", fill: "Yellow"});
-    
-        
-        // add 2 animations for physics
-        this.anims.create({
-            key: "red",
-            frames: this.anims.generateFrameNumbers("power-up", {
-                start: 0,
-                end: 1
-            }),
-            frameRate: 26,
-            repeat: -1
-        });
-
-        this.anims.create({
-            key: "gray",
-            frames: this.anims.generateFrameNumbers("power-up", {
-                start: 2,
-                end: 3
-            }),
-            frameRate: 26,
-            repeat: -1
-        });
-
-        // Power Ups Bag
-        this.powerUps = this.physics.add.group();
-
-        // define how many powerups we want in for loop. (how I'll display meteros/comets)
-        let maxObjects = 4;
-        for (let i = 0; i <= maxObjects; i++) {
-            let powerUp = this.physics.add.sprite(16, 16, "power-up");
-            this.powerUps.add(powerUp);
-            powerUp.setRandomPosition(0, 0, game.config.width, game.config.height);
-        
-        
-            // random animations for powerups
-            if(Math.random() > 0.5) {
-                powerUp.play("red");
-            } else {
-                powerUp.play("gray");
-            }
-
-
-            // Set Velocity of powerups // change velocity
-            powerUp.setVelocity(100, 100);
-
-            // Set Barrier/Boundaries for powerups
-            powerUp.setCollideWorldBounds(true);
-
-            // Make the powerups bounce
-            powerUp.setBounce(1);
-
-
-        }
+        // Create group to hold all our projectiles
+        this.projectiles = this.add.group();
     
     }
 
     // update function
     update() {
         // rotate image continuesly
-        this.ship1.angle += 2;
+        //this.ship1.angle += 2;
 
 
         // Move ship continuesly
-        this.moveShip(this.ship1, 1);
-        this.moveShip(this.ship2, 2);
-        this.moveShip(this.ship3, 3);
+        //this.moveShip(this.ship1, 1);
+        //this.moveShip(this.ship2, 2);
+        //this.moveShip(this.ship3, 3);
 
         // Move the background tilesprite y position so it looks like moving!
         this.background.tilePositionY -= 0.5;
+
+        // Call function to control the player's ship
+        this.movePlayerManager();
+
+        // Player ship shoot
+        if(Phaser.Input.Keyboard.JustDown(this.spacebar)) {
+            //console.log("Fire!");
+            // Call shootBeam function!
+            this.shootBeam();
+        }
+
+        // Iterate through each element of projectile group and update it!
+        for(let i = 0; i < this.projectiles.getChildren().length; i++) {
+            let beam = this.projectiles.getChildren()[i];
+            beam.update();
+        }
+
+        
+
     }
 
 
@@ -190,6 +102,38 @@ class Scene2 extends Phaser.Scene {
     destroyShip(pointer, gameObject) {
         gameObject.setTexture("explosion");
         gameObject.play("explode");
+    }
+
+    // movePlayerManager Callback function (To move player's ship!)
+    movePlayerManager() {
+        
+        // Move Horizontally
+        if(this.cursorKeys.left.isDown) {
+            this.player.setVelocityX(-gameSettings.playerSpeed);
+        } else if(this.cursorKeys.right.isDown) {
+            this.player.setVelocityX(gameSettings.playerSpeed);
+        }
+
+        // Move Vertically
+        if(this.cursorKeys.up.isDown) {
+            this.player.setVelocityY(-gameSettings.playerSpeed);
+        } else if(this.cursorKeys.down.isDown) {
+            this.player.setVelocityY(gameSettings.playerSpeed);
+        }
+
+
+
+
+    }
+
+
+    // shootBeam function to shoot the beam. UwU
+    shootBeam() {
+        //let beam = this.physics.add.sprite(this.player.x, this.player.y, "beam");
+    
+        // Create variable "Beam" from the "Class Beam" and pass the scene as parameter.
+        let beam = new Beam(this);
+    
     }
 
 
